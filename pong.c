503c1166cde6ee8da29d095d5d7548e3d2ec424c
Jonna Binns <jonnabin@se-f4.nsk.21-school.ru>
2024-01-27 16:28:53 +0700

    game cycle

--- pong.c
+++ pong.c
@@ -9,23 +9,28 @@ enum {
 
     BALL_INIT_COLUMN = BORDER_LENGTH / 2,
     BALL_INIT_ROW = BORDER_WIDTH / 2,
+    P_INIT_ROW = BORDER_WIDTH / 2,
 };
 
 const char PLAYER = '|';
 const char BALL = '$';
 const char BORDER = '#';
 const char SPACE = ' ';
+const char NET = '|';
 
 void print_frame(int ball_column, int ball_row, int p1_row, int p2_row);
 
 int main(void) {
     int ball_column = BALL_INIT_COLUMN;
     int ball_row = BALL_INIT_ROW;
+    int key_pressed;
 
-
-    print_frame(ball_column, ball_row, 6, 6);
-    // printf("\e[1;1H\e[2J");
-
+    while(key_pressed != 'q') {
+        print_frame(ball_column, ball_row, P_INIT_ROW, P_INIT_ROW);
+        key_pressed = getchar();
+        printf("\e[1;1H\e[2J");
+    }
+    
     return 0;
 }
 
@@ -38,6 +43,8 @@ void print_frame(int ball_column, int ball_row, int p1_row, int p2_row) {
                 printf("%c", BALL);
             } else if ((row >= p1_row-1 && row <= p1_row+1 && col == P1_COLUMN) || (row >= p2_row-1 && row <= p2_row+1 && col== P2_COLUMN)) {
                 printf("%c", PLAYER);
+            } else if(col == BALL_INIT_COLUMN) {
+                printf("%c", NET);
             } else {
                 printf("%c", SPACE);
             }
