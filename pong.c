e94bd3b0708956fd667fb0a4313d12280b157404
Jonna Binns <jonnabin@ge-a2.nsk.21-school.ru>
2024-01-28 00:59:10 +0700

    code refactoring, add types

--- pong.c
+++ pong.c
@@ -15,81 +15,135 @@ enum {
     P1_SCORE_POS2 = 31,
     P2_SCORE_POS1 = 49,
     P2_SCORE_POS2 = 50,
-}; 
 
-const char BALL = '$';
-const char BORDER = '#';
-const char NET = '|';
-const char PLAYER = '|';
-const char SPACE = ' ';
+    WIN_SCORE = 21,
+};
+
+typedef struct {
+    int x;
+    int y;
+    int x_vector;
+    int y_vector;
+} game_ball;
+
+typedef struct {
+    int p1;
+    int p2;
+} game_score;
+
+const char* BALL = "\e[39;41m ";
+const char* BORDER = "\e[32;46m ";
+const char* NET = "\e[39;100m ";
+const char* PLAYER = "\e[39;42m ";
+const char* SPACE = " ";
 
 void cls(void);
+void print_frame(game_ball ball, int p1_row, int p2_row, game_score score);
+
 void game_control(void);
-
-void print_frame(int ball_column, int ball_row, int p1_row, int p2_row, int p1_score, int p2_score);
-
 int get_x_direction(int x_position, int y_position, int x_direction, int p1_pos, int p2_pos);
 int get_y_direction(int y_position, int y_direction);
 
+void hide_cursor(void);
+void show_cursor(void);
+void set_default_color(void);
+
+void win_scr1(void);
+void win_scr2(void);
+
+game_ball ball_movement(game_ball ball, int p1_row, int p2_row);
+
+_Bool is_round_won(game_ball ball) { return ball.x == 1 || ball.x == BORDER_LENGTH - 1; }
+
+game_score upd_score(game_score score, game_ball ball) {
+    ball.x == 1 ? ++score.p2 : ++score.p1;
+    return score;
+}
+
+game_ball ball_reset(game_ball ball) {
+    ball.x = BALL_INIT_COLUMN;
+    ball.y = BALL_INIT_ROW;
+    ball.x_vector *= -1;
+    return ball;
+}
+
 int main(void) {
-    int ball_column = BALL_INIT_COLUMN;
-    int ball_row = BALL_INIT_ROW;
-    int ball_x_direction = 1;
-    int ball_y_direction = 1;
-
     int key_pressed;
-    int p1_score = P_INIT_SCORE;
-    int p2_score = P_INIT_SCORE;
-
+    game_score score = {P_INIT_SCORE, P_INIT_SCORE};
     int p1_row = P_INIT_ROW;
     int p2_row = P_INIT_ROW;
 
-    while (key_pressed != 'q') {
-        cls();
-        print_frame(ball_column, ball_row, P_INIT_ROW, P_INIT_ROW, p1_score, p2_score);
+    hide_cursor();
 
-        ball_x_direction = get_x_direction(ball_column, ball_row, ball_x_direction, p1_row, p2_row);
-        ball_y_direction = get_y_direction(ball_row, ball_y_direction);
-        ball_column += ball_x_direction;
-        ball_row += ball_y_direction;
+    game_ball ball = {BALL_INIT_COLUMN, BALL_INIT_ROW, 1, 1};
+
+    do {
+        cls();
+        print_frame(ball, P_INIT_ROW, P_INIT_ROW, score);
+        ball = ball_movement(ball, p1_row, p2_row);
+
+        if (is_round_won(ball)) {
+            score = upd_score(score, ball);
+            ball = ball_reset(ball);
+        }
 
         key_pressed = getchar();
+    } while (score.p1 != 21 && score.p2 != 21 && key_pressed != 'q' && key_pressed != 'Q');
+
+    cls();
+    if (score.p1 == WIN_SCORE || score.p2 == WIN_SCORE) {
+        printf("\e[32;49m");
+        score.p1 == WIN_SCORE ? win_scr1() : win_scr2();
     }
 
+    show_cursor();
+    set_default_color();
     return 0;
 }
 
-void cls(void) { printf("\e[1;1H\e[2J"); }
-
-void print_score(int column, int p1_score, int p2_score) {
-    if (column == P1_SCORE_POS1)
-        printf("%c", (p1_score / 10) + '0');
-    else if (column == P1_SCORE_POS2)
-        printf("%c", (p1_score % 10) + '0');
-    else if (column == P2_SCORE_POS1)
-        printf("%c", (p2_score / 10) + '0');
-    else if (column == P2_SCORE_POS2)
-        printf("%c", (p2_score % 10) + '0');
+game_ball ball_movement(game_ball ball, int p1_row, int p2_row) {
+    ball.x_vector = get_x_direction(ball.x, ball.y, ball.x_vector, p1_row, p2_row);
+    ball.y_vector = get_y_direction(ball.y, ball.y_vector);
+    ball.x += ball.x_vector;
+    ball.y += ball.y_vector;
+    return ball;
 }
 
-void print_frame(int ball_column, int ball_row, int p1_row, int p2_row, int p1_score, int p2_score) {
+void cls(void) { printf("\e[1;1H\e[2J"); }
+void hide_cursor(void) { printf("\e[?25l"); }
+void show_cursor(void) { printf("\e[?25h"); }
+void set_default_color(void) { printf("\e[39;49m"); };
+
+void print_score(int column, game_score score) {
+    if (column == P1_SCORE_POS1)
+        printf("%c", (score.p1 / 10) + '0');
+    else if (column == P1_SCORE_POS2)
+        printf("%c", (score.p1 % 10) + '0');
+    else if (column == P2_SCORE_POS1)
+        printf("%c", (score.p2 / 10) + '0');
+    else if (column == P2_SCORE_POS2)
+        printf("%c", (score.p2 % 10) + '0');
+}
+
+void print_frame(game_ball ball, int p1_row, int p2_row, game_score score) {
     for (int row = 0; row < BORDER_WIDTH; ++row) {
         for (int col = 0; col < BORDER_LENGTH; ++col) {
             if ((row == 0 || row == BORDER_WIDTH - 1) || (col == 0 || col == BORDER_LENGTH - 1)) {
-                printf("%c", BORDER);
-            } else if (row == ball_row && col == ball_column) {
-                printf("%c", BALL);
+                printf("%s", BORDER);
+            } else if (row == ball.y && col == ball.x) {
+                printf("%s", BALL);
             } else if ((row >= p1_row - 1 && row <= p1_row + 1 && col == P1_COLUMN) ||
                        (row >= p2_row - 1 && row <= p2_row + 1 && col == P2_COLUMN)) {
-                printf("%c", PLAYER);
+                printf("%s", PLAYER);
             } else if (row == 2 && (col == P1_SCORE_POS1 || col == P1_SCORE_POS2 || col == P2_SCORE_POS1 ||
                                     col == P2_SCORE_POS2)) {
-                print_score(col, p1_score, p2_score);
+                print_score(col, score);
             } else if (col == BALL_INIT_COLUMN) {
-                printf("%c", NET);
+                printf("%s", NET);
             } else {
-                printf("%c", SPACE);
+                printf("%s", SPACE);
             }
+            set_default_color();
         }
         printf("\n");
     }
@@ -108,4 +162,24 @@ int get_y_direction(int y_position, int y_direction) {
     int direction = y_direction;
     if (y_position == (BORDER_WIDTH - 2) || y_position == 1) direction *= -1;
     return direction;
+}
+
+void win_scr1(void) {
+    printf("        ########     ##      ##      ## #### ##    ##\n");
+    printf("        ##     ##  ####      ##  ##  ##  ##  ###   ##\n");
+    printf("        ##     ##    ##      ##  ##  ##  ##  ####  ##\n");
+    printf("        ########     ##      ##  ##  ##  ##  ## ## ##\n");
+    printf("        ##           ##      ##  ##  ##  ##  ##  ####\n");
+    printf("        ##           ##      ##  ##  ##  ##  ##   ###\n");
+    printf("        ##         ######     ###  ###  #### ##    ##");
+}
+
+void win_scr2(void) {
+    printf("        ########   #######     ##      ## #### ##    ##\n");
+    printf("        ##     ## ##     ##    ##  ##  ##  ##  ###   ##\n");
+    printf("        ##     ##        ##    ##  ##  ##  ##  ####  ##\n");
+    printf("        ########   #######     ##  ##  ##  ##  ## ## ##\n");
+    printf("        ##        ##           ##  ##  ##  ##  ##  ####\n");
+    printf("        ##        ##           ##  ##  ##  ##  ##   ###\n");
+    printf("        ##        #########     ###  ###  #### ##    ##");
 }
